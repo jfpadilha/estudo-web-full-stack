2a496d939387e8494176916b0d8934fbc0ddf46f
Joao F P S <jfpadilha@gmail.com>
2021-02-12 01:41:20 -0300

    Ajuste final no push com tags

--- push.sh
+++ push.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+#!/bin/bash
 echo " "
 echo "----- Bitbucket ||"
 git push originbitbucket master
 echo " "
 echo "----- Github ||"
 git push origingithub master
-
+echo " "
+echo "-----Tags Bitbucket ||"
+git push --tags originbitbucket master
+echo " "
+echo "----- Tags Github ||"
+git push --tags origingithub master
