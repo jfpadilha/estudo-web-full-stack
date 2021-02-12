780bef350e234644544d396f3212c65f37f8c834
Joao F P S <jfpadilha@gmail.com>
2021-02-12 01:25:43 -0300

    ajuste final no push.sh

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo " "
 echo "----- Bitbucket ||"
 git push originbitbucket master
+echo " "
 echo "----- Github ||"
 git push origingithub master
-echo "----- Push tags ||"
-git push --tags
+
